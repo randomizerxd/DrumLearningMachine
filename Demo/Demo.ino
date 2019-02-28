811c67d9f7e272c87a0e481edc7bef8e48aae077
ALbertoCervantes <albertocervantes96@csu.fullerton.edu>
2019-02-28 15:36:12 -0800

    Added VolumeCtrl() function, cleaned code, set proper limits for tempo, created an adjusted tempo (true tempo), added new beat

--- Demo/Demo.ino
+++ Demo/Demo.ino
@@ -1,12 +1,16 @@
 
 //INITIALIZING PINS AND VARIABLES
-int kick = 29;   //setting pin 29 to 'kick' sound
-int snare = 30;  //setting pin 30 to 'snare' sound
-int hihat = 28;  //setting pin 28 to 'hihat' sound
-int hhkick = 31; //setting pin 31 to 'hihat' and 'kick' sound
-int hhsnare = 32;//setting pin 32 to 'hihat' and 'snare' sound
-int tempo = 200;  //316 = 190bpm; Inc = slower; Dec = faster (LIMIT: 83ms to 475ms)
+int hihat     = 28;        //set pin 28 to 'hihat' sound
+int kick      = 29;        //set pin 29 to 'kick' sound
+int snare     = 30;        //set pin 30 to 'snare' sound
+int hhkick    = 31;        //set pin 31 to 'hihat' and 'kick' sound
+int hhsnare   = 32;        //set pin 32 to 'hihat' and 'snare' sound
 
+int volUp     = 34;        //set pin 34 to volume-up
+int volDown   = 35;        //set pin 35 to volume-down
+
+int tempo     = 500;       //316 = 190bpm; Inc = slower; Dec = faster (LIMIT: 350ms to 1500ms)
+int adj_tempo = tempo-250; //adjusted tempo to take into account the file delay
 
 void setup() {
   //setup code here, to run once:
@@ -16,18 +20,19 @@ void setup() {
   //T01 = snare
   //T02 = kick
   //T03 = kick and hihat
-  pinMode(kick, OUTPUT);
-  pinMode(snare, OUTPUT);
-  pinMode(hihat, OUTPUT);
-  pinMode(hhkick, OUTPUT);
+  pinMode(kick,    OUTPUT);
+  pinMode(snare,   OUTPUT);
+  pinMode(hihat,   OUTPUT);
+  pinMode(hhkick,  OUTPUT);
   pinMode(hhsnare, OUTPUT);
+  RESET(); //clean start
 }
 
 void loop() {
-  
+
+  rockYou();    //we will - we will - rock you TA - BUM BUM TA
   //rockBeat();
-  discoBeat();
-  //reggaetonBeat();
+  //discoBeat();
 
 }
 
@@ -38,10 +43,10 @@ void loop() {
 
   //Initializing pins to 5V so they do NOT play sound
 void RESET(){
-  digitalWrite(kick, HIGH);
-  digitalWrite(snare, HIGH);
-  digitalWrite(hihat, HIGH);
-  digitalWrite(hhkick, HIGH);
+  digitalWrite(kick,    HIGH);
+  digitalWrite(snare,   HIGH);
+  digitalWrite(hihat,   HIGH);
+  digitalWrite(hhkick,  HIGH);
   digitalWrite(hhsnare, HIGH);
   //When values are set to 0, the sound will be played
 }
@@ -51,7 +56,19 @@ void playSound(int part){
   digitalWrite(part, LOW);  //starts playback of file
   delay(250);               //plays file for the appropriate amount of time
   RESET();                  //stops playback of file
-  delay(tempo);             //moves on to next file for the appropriate tempo
+  delay(adj_tempo);         //moves on to next file for the appropriate tempo
+}
+
+  //Volume Control
+void VolumeCtr(int button){
+  //Values that come through the app
+  int downButton = 0; //(-)
+  int upButton   = 1; //(+)
+  
+  if (button == downButton)
+    volDown = LOW;
+  if (button == upButton)
+    volUp   = LOW;  
 }
 
 
@@ -78,7 +95,7 @@ void rockBeat(){
 /*                    DISCO BEAT                      */
 /******************************************************/
 
-void discoBeat(){  
+void discoBeat(){
   playSound(kick);
   
   playSound(hihat);
@@ -88,6 +105,19 @@ void discoBeat(){
   playSound(hihat);
 }
 
+/******************************************************/
+/*                   ROCK-YOU BEAT                    */
+/******************************************************/
+
+void rockYou(){  
+  playSound(kick);
+  
+  playSound(kick);
+  
+  playSound(snare);
+  
+  playSound(0);
+}
 
 /******************************************************/
 /*                  REGGAETON BEAT                    */

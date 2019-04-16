6f14af779993a8dd5190994f36d0b3641f55dd31
ALbertoCervantes <albertocervantes96@csu.fullerton.edu>
2019-04-16 15:39:04 -0700

    Changed code and added code to get something going; function that is going to be template is hihat_kick(). We will use COUNT variable to offer a score at the end of the program.

--- PlayAlong/PlayAlong.ino
+++ PlayAlong/PlayAlong.ino
@@ -32,11 +32,36 @@ const int tomREDPin     = A14;
 const int tomBLUEPin    = A15;
 
 /***Variables to store the value read from the sensor pin***/
-int hihatSensorReading = LOW;
-int snareSensorReading = LOW;
-int kickSensorReading  = LOW;
-int crashSensorReading = LOW;
-int tomSensorReading   = LOW;
+short hihatSensorReading = LOW;
+short snareSensorReading = LOW;
+short kickSensorReading  = LOW;
+short crashSensorReading = LOW;
+short tomSensorReading   = LOW;
+
+const short threshold_hihat = 100; 
+const short threshold_snare = 94; 
+const short threshold_kick = 130; 
+
+/***Variables used in the 'averageAnalogRead()' function***/
+short n = 500;  //amount of times to read from both hihat and snare
+long t0;        //initial value of t
+long t;         //stores the amount of time it takes to finish the for loop
+short n_hihat = n;    //used to average. n_hihat <= n. It will be decremented by 1 when sensor readings don't exceed the threshold
+short n_snare = n;    //used to average. n_snare <= n. It will be decremented by 1 when sensor readings don't exceed the threshold
+short n_kick = n;    //used to average. n_kick <= n. It will be decremented by 1 when sensor readings don't exceed the threshold
+float hihatSensorReading_Average = 0;
+float snareSensorReading_Average = 0;
+float kickSensorReading_Average  = 0;
+short hihatSensorReading_tmp = 0; //local variable. Don't change 'hihatSensorReading'
+short snareSensorReading_tmp = 0; //local variable. Don't change 'snareSensorReading'
+short kickSensorReading_tmp = 0; //local variable. Don't change 'kickSensorReading'
+
+const short threshold = 200;
+
+/***Variables for the FINAL SCORE***/
+int TIME = millis();
+int COUNT = 0;
+int tempo = 430;
 
 void setup() {
   Serial.begin(9600);   //use serial port
@@ -70,8 +95,6 @@ void loop() {
    *      iv. turn off light
    *      
    */
-
-   int HitCount;
 
 
    
@@ -150,6 +173,15 @@ void initializeLEDstrips() {
   analogWrite(tomBLUEPin, 0);
 }
 
+//Play part of Drumset
+void startCheck(int count){
+  TIME = millis();  //start count for hits
+  //check reading
+  delay(250);               //plays file for the appropriate amount of time
+  RESET();                  //stops playback of file
+  delay(adj_tempo);         //moves on to next file for the appropriate tempo
+}
+
 /******************************************************/
 /*        FUNCTIONS FOR THE DIFFERENT BEATS           */
 /******************************************************/
@@ -163,21 +195,130 @@ void initializeLEDstrips() {
 /******************************************************/   
 
 void rockBeat(){  //Setting LEDs to specific colors/pins
+  hihat_kick();
+  startCheck(COUNT);
+
+  hihat();
+  startCheck(COUNT);
+
+  hihat_snare(); 
+  startCheck(COUNT);
+
+  hihat();  
+  startCheck(COUNT);
+}
+
+
+
+/* SEQUENCE CODE */
+/******************************************************/
+
+void hihat() {
+  analogWrite(hihatREDPin, 255);
+  while( analogRead(hihatSensorPin) < threshold_hihat) { } //Do nothing until hihat sensor passes threshold
+  hitConfirmation_hihat();
+}
+
+void snare() {
+  analogWrite(snareBLUEPin, 255);
+  while( analogRead(snareSensorPin) < threshold_snare) { } //Do nothing until snare sensor passes threshold
+  hitConfirmation_snare();
+}
+
+void kick() {
+  analogWrite(kickREDPin, 255);
+  analogWrite(kickGREENPin, 128);
+  while( analogRead(kickSensorPin) < threshold_kick) { } //Do nothing until kick sensor passes threshold
+  hitConfirmation_kick();
+}
+
+//This function turns on the lights on the hihat and kick and waits for the user to hit both of them
+void hihat_kick() {
   analogWrite(hihatREDPin, 255);
   analogWrite(kickREDPin, 255);
   analogWrite(kickGREENPin, 128);
-  playSound(hhkickFXPin);
-
-  analogWrite(hihatREDPin, 255);
-  playSound(hihatFXPin);
-
-  analogWrite(hihatREDPin, 255);
-  analogWrite(snareBLUEPin, 255);  
-  playSound(hhsnareFXPin);
-
-  analogWrite(hihatREDPin, 255);  
-  playSound(hihatFXPin);
+  TIME = millis();
+  int diffTime = 0;
+  int tmpCount = 0;
+  while (diffTime < tempo){
+    if (((hihatSensorReading = analogRead(hihatSensorPin)) < threshold_hihat) && ((kickSensorReading = analogRead(kickSensorPin)) < threshold_kick)){
+      //do nothing if below threshold
+    }
+    averageAnalogRead_hihatkick(); 
+       
+    if(((hihatSensorReading_Average) > threshold_hihat) && ((kickSensorReading_Average) > threshold_kick)) {
+      tmpCount++;
+    } 
+    diffTime = millis() - TIME;
+  }
+  if (tmpCount > 0){
+    COUNT++;
+  }
 }
+
+
+  
+
+//This function turns on the lights on the hihat and snare and waits for the user to hit both of them
+void hihat_snare() {
+  analogWrite(hihatREDPin, 255);
+  analogWrite(snareBLUEPin, 255);
+  while( ((hihatSensorReading = analogRead(hihatSensorPin)) < threshold_hihat) && ((snareSensorReading = analogRead(snareSensorPin)) < threshold_snare) ) {  
+  }
+  
+  averageAnalogRead_hihatsnare();
+  
+  if( ( (hihatSensorReading_Average) > threshold_hihat) && ( (snareSensorReading_Average) > threshold_snare) ) {
+    hitConfirmation_hihatsnare();
+  } 
+  else if( hihatSensorReading >= threshold_hihat ) {    //If the hihat sensor was triggered
+    hitConfirmation_hihat(); 
+    while( (snareSensorReading = analogRead(snareSensorPin)) < threshold_snare) { } //Do nothing until snare is hit
+    hitConfirmation_snare();
+  }
+  else if ( snareSensorReading >= threshold_snare ) { //If the snare sensor was triggered
+    hitConfirmation_snare(); 
+    while( analogRead(hihatSensorPin) < threshold_hihat) { } //Do nothing until hihat sensor is pushed
+    hitConfirmation_hihat();
+  } 
+}
+
+
+
+
+
+/**********************************************************/
+/*           Check if Proper Hit Function                 */
+/**********************************************************/
+
+int checkCountHit(int startTime, int CountHit){ //should pass in TIME
+  int endTime = millis();
+  int difference = endTime - startTime;
+  
+  while(difference < ){
+   if (reading > threshold)
+      countHit++;
+    difference = micros() - start_time;
+  }
+  Serial.print("Difference in time: ");
+  Serial.print(difference);
+  
+  return CountHit;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// down here for now
 
 /******************************************************/
 /*                    ROCK V2 BEAT                    */
@@ -262,26 +403,4 @@ void rockYou(){
   playSound(snareFXPin);
 
   playSound(0);
-}
-
-/**********************************************************/
-/*           Check if Proper Hit Function                 */
-/**********************************************************/
-
-int checkCountHit(){
-  int countHit = 0;
-  int reading = 0;
-  int threshold = 10;
-  int start_time = millis();
-  int difference = start_time;
-  
-  while(difference < 100){
-   if (reading > threshold)
-      countHit++;
-    difference = micros() - start_time;
-  }
-  Serial.print("Difference in time: ");
-  Serial.print(difference);
-  
-  return CountHit;
 }
